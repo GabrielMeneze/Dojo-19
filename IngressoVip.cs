dbe8aa27ccd65d0eff09bea1a37cca60aa8c9d6c
Gabrieldevs77 <petollolmel@gmail.com>
2020-06-17 10:25:24 -0300

    Finalizado

---
+++ IngressoVip.cs
@@ -0,0 +1,13 @@
+namespace Dojo_19
+{
+    public class IngressoVip : Ingresso
+    {
+        public float ValorVip=200;
+
+        public void MostrarValorVip()
+        {
+            float resultado = Valor + ValorVip;
+            System.Console.WriteLine("Valor do ingresso vip é: {0:c}",resultado);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Dojo_19
+{
+    class Program
+    {                              
+        static void Main(string[] args)
+        {
+            IngressoVip ing2 = new IngressoVip();
+            Ingresso ing = new Ingresso();
+            IngressoInfantil ing3 = new IngressoInfantil();
+            ing.Valor = 110;                                   
+            ing2.ValorVip = 90;
+            ing3.IngressoInfan = 55;
+            ing.MostrarValor();
+            ing2.MostrarValorVip();
+            ing3.MostrarInfan();
+        }
+    }
+}
